58107d3e2b51568da6918d08b2e259aa798c3992
jannunzi <jannunzi@gmail.com>
2020-03-09 20:31:38 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'wbdv-online-sp20-jannunzi-client-angular';
+}
